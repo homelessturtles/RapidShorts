5a707139fb17209be85cbdcf65adc0471b6b212f
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-02-26 14:27:34 -0800

    create prompt form request and endpoint

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+
+prompttxt = None
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', prompttxt=prompttxt)
+
+
+@app.route("/prompt", methods=["POST"])
+def prompt():
+    global prompttxt
+    prompttxt = request.form['prompttxt']
+    return redirect(url_for("index"))
+
 
 app.run(host="0.0.0.0", port=80, debug=True)
