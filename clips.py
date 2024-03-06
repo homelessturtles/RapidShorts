85df769d6fe16ad2ba8aaa441c84f278aad916f2
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-03-06 15:23:50 -0800

    define parse/source functions for clips.py

---
+++ clips.py
@@ -0,0 +1,6 @@
+def parse_script(scripttext):
+    '''create map for each scene and its corresponding keywords'''
+
+
+def source_clips_pexley():
+    '''make api call to pexley, return video clips'''
