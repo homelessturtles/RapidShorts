7479307dc4e241e6ab27cfc4cfc298846fbcdb28
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-03-06 16:46:32 -0800

    change name from clips.py to clips_gen.py

--- clips.py
+++
@@ -1,6 +0,0 @@
-def parse_script(scripttext):
-    '''create map for each scene and its corresponding keywords'''
-
-
-def source_clips_pexley():
-    '''make api call to pexley, return video clips'''
