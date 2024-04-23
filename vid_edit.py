6c40ef8c4f7d4dd15b10a5cda802132dd88c87e0
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-04-22 21:57:57 -0700

    test assets add and audio length function

--- vid_edit.py
+++ vid_edit.py
@@ -1,16 +1,46 @@
 from moviepy.editor import *
-from mutagen.mp3 import MP3
+import soundfile as sf
 
 scenes_dict = {
-    'scene1': {'clips: []'}
+    'scene1': {'clips': ['Test_Assets/family_1.mp4', 'Test_Assets/family_2.mp4'], 'narration': 'Test_Assets/scene1.mp3'},
+    'scene2': {'clips': ['Test_Assets/nature_1.mp4', 'Test_Assets/nature_2.mp4'], 'narration': 'Test_Assets/scene2.mp3'},
+    'scene3': {'clips': ['Test_Assets/lions_1.mp4', 'Test_Assets/lions_2.mp4'], 'narration': 'Test_Assets/scene3.mp3'}
 }
 
-def get_audio_length(path):
+def get_audio_length(filename):
     try:
-        audio = MP3(path)
-        length = audio.info.length
-        return length
-    except:
+        audio_data, sample_rate = sf.read(filename)
+        length_in_seconds = len(audio_data) / sample_rate
+        return length_in_seconds
+    except Exception as e:
+        print("Error:", e)
         return None
     
-def trim_clips(clips_dict): pass
+def edit_clips(scenes_dict): 
+    for k,v in scenes_dict:
+        clip_1 = VideoFileClip(scenes_dict[k]['clips'][0])
+        clip_2 = VideoFileClip(scenes_dict[k]['clips'][1])
+        narration = scenes_dict[k]['narration']
+        narration_length = get_audio_length(narration)
+
+
+def concatenate_clips(clip1, clip2, duration):
+    duration_clip1 = clip1.duration
+    duration_clip2 = clip2.duration
+
+    if duration >= duration_clip1 + duration_clip2:
+        final_clip = concatenate_videoclips([clip1, clip2])
+    elif duration < duration_clip1:
+        final_clip = clip1.subclip(0, duration)
+    else:
+        final_clip = concatenate_videoclips([clip1, clip2.subclip(0, duration - duration_clip1)])
+
+    return final_clip
+
+clip1 = VideoFileClip(scenes_dict['scene1']['clips'][0])
+clip2 = VideoFileClip(scenes_dict['scene1']['clips'][1])
+scene1_audio_length = get_audio_length('Test_Assets/scene1.mp3')
+test = concatenate_clips(clip1, clip2, scene1_audio_length)
+test.write_videofile('test_video.mp4', codec='libx264', fps=24)
+        
+
--- voice_gen.py
+++ voice_gen.py
@@ -1,8 +1,16 @@
 import pyttsx3
 
-def create_speech_file(speech):
+def create_speech_file(speech, name):
     engine = pyttsx3.init()
-    engine.save_to_file(speech, 'test.mp3')
+    engine.save_to_file(speech, f'{name}.mp3')
     engine.runAndWait()
+
+
+create_speech_file('Feeling scatterbrained and lacking focus? Say goodbye to distractions and hello to enhanced productivity with our mushroom supplement.', 'scene1')
+
+create_speech_file('Watch as your energy levels soar and your mind gains crystal-clear clarity with our powerful mushroom blend.', 'scene2')
+
+create_speech_file('Experience a natural boost in productivity and focus, without the crash or jitters that come with synthetic alternatives.', 'scene3')
+
 
 
