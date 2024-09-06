883a69fb2138d0627af96fab8c3f07bb0d66c19e
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-09-05 22:37:00 -0700

    remove parse function in scriptgen

--- backend/script_gen.py
+++ backend/script_gen.py
@@ -11,21 +11,7 @@ client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
 
-
 sample_prompt = 'how to make an iphone application'
-
-
-def format_sample_output(txt_output):
-    with open(txt_output) as file:
-        sample_output = file.readlines()
-    return sample_output
-
-
-def generate_script_mock(prompt):
-    '''returns a pre defined generated script'''
-
-    return f"prompt: {prompt}\nscript:{format_sample_output('script.txt')}"
-
 
 #returns scenes in json format
 def generate_script(topic):
@@ -67,44 +53,5 @@ def generate_script(topic):
         ]
     )
     return json.loads(completion.choices[0].message.content)
-
-
-def parse_script(script):
-    scenes_dict = {}
-    # Split the input string into scenes
-    scenes = script.strip().split('\n\n')
-
-    # Initialize lists to store narrations and keywords for each scene
-    narrations = []
-    keywords = []
-
-    # Parse each scene to extract narration and keywords
-    for scene in scenes:
-        scene_lines = scene.split('\n')
-
-        scene_narration = ""
-        scene_keywords = ""
-
-        # Iterate through the lines to find keywords and narration
-        for line in scene_lines:
-            if line.startswith("Keywords:"):
-                scene_keywords = line.replace('Keywords: ', '').strip()
-            elif line.startswith("Narrator:"):
-                scene_narration = line.replace('Narrator: ', '').strip()
-            else:
-                continue
-
-        # Append narration and keywords to the respective lists
-        narrations.append(scene_narration)
-        keywords.append(scene_keywords)
-
-    for i, (narration, keyword) in enumerate(zip(narrations, keywords), start=1):
-        item = {f'scene_{i}': {
-            'keywords': keyword,
-            'narration': narration
-        }}
-        scenes_dict.update(item)
-
-    return scenes_dict
 
 
