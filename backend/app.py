97dd96d10337b907096ea5e8c0aac3a5d2b7f3b3
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-09-21 15:37:29 -0700

    index path is our react app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from test import test
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+@app.route("/", defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename="index.html"
+    return send_from_directory(dist_folder,filename)
 
 @app.route("/api/process_prompt", methods=['POST'])
 def index():
